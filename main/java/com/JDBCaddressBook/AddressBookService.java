3a904207db5b8e24ae805c9583b94e64b10f7556
suyash-dce <jain.suyash28@gmail.com>
2020-11-02 15:18:37 +0530

    Added: Address Book Service is Added for Sync Data with DB

---
+++ main/java/com/JDBCaddressBook/AddressBookService.java
@@ -0,0 +1,49 @@
+import java.util.List;
+import com.bridgelabz.main.AddressBookDBException.ExceptionType;
+
+public class AddressBookService {
+	public enum IOService {
+		CONSOLE_IO, FILE_IO, DB_IO, REST_IO, CSV, JSON
+	}
+
+	private List<Contact> contactList;
+	private AddressBookJDBCServices addressBookJDBCServices;
+
+	public AddressBookService() {
+		addressBookJDBCServices = AddressBookJDBCServices.getInstance();
+	}
+
+	public AddressBookService(List<Contact> employeePayrollList) {
+		this();
+		this.contactList = employeePayrollList;
+	}
+
+	public List<Contact> readContactData(IOService ioService) {
+		if (ioService.equals(IOService.FILE_IO))
+			contactList = new FileIO().readData();
+		if (ioService.equals(IOService.DB_IO))
+			contactList = addressBookJDBCServices.readData();
+		return contactList;
+	}
+
+	public void updateCity(String firstName, String city) throws AddressBookDBException {
+		int result = addressBookJDBCServices.updateContactUsingSQL(firstName, "city", city);
+		Contact contact = getContactData(firstName);
+		if (result != 0 && contact != null)
+			contact.setCity(city);
+		if (result == 0)
+			throw new AddressBookDBException("Wrong name given", ExceptionType.WRONG_NAME);
+		if (contact == null)
+			throw new AddressBookDBException("No data found", ExceptionType.NO_DATA_FOUND);
+	}
+
+	private Contact getContactData(String name) {
+		readContactData(IOService.DB_IO);
+		return contactList.stream().filter(e -> e.getFirstName().equals(name)).findFirst().orElse(null);
+	}
+
+	public boolean isAddressBookSyncedWithDB(String firstName) {
+		Contact contact = getContactData(firstName);
+		return addressBookJDBCServices.getContacts(firstName).get(0).equals(contact);
+	}
+}
