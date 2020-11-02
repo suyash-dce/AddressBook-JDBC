cbfd1cbb9cb66a9dffcfee99e93eb0d364f4cf4c
suyash-dce <jain.suyash28@gmail.com>
2020-11-02 15:20:51 +0530

    Updated: DB Service to Update Any Record in addressBook DB

--- main/java/com/JDBCaddressBook/AddressBookJDBCServices.java
+++ main/java/com/JDBCaddressBook/AddressBookJDBCServices.java
@@ -35,4 +35,42 @@ public class AddressBookJDBCServices{
 		String password = "jain1234";
 		return DriverManager.getConnection(jdbcURL, userName, password);
 	}
+	
+	public List<Contact> readData() {
+		String sql = String.format("select * from address_book");
+		return getContactList(sql);
+	}
+
+	private List<Contact> getContactList(String sql) {
+		List<Contact> contactList = new ArrayList<>();
+		try (Connection connection = getConnection()) {
+			PreparedStatement preparedStatement = connection.prepareStatement(sql);
+			ResultSet resultSet = preparedStatement.executeQuery();
+			while (resultSet.next()) {
+				contactList.add(new Contact(resultSet.getString("first_name"), resultSet.getString("last_name"),
+						resultSet.getString("address"), resultSet.getString("city"), resultSet.getString("state"),
+						resultSet.getString("zip"), resultSet.getString("phone_number"), resultSet.getString("email")));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return contactList;
+	}
+	
+	public List<Contact> getContacts(String firstName) {
+		String sql = String.format("select * from address_book where first_name = '%s'", firstName);
+		return getContactList(sql);
+	}
+
+	public int updateContactUsingSQL(String firstName, String column, String columnValue) {
+		String sql = String.format("UPDATE address_book SET %s = '%s'  WHERE first_name = '%s';", column, columnValue,
+				firstName);
+		try (Connection connection = getConnection()) {
+			PreparedStatement preparedStatement = connection.prepareStatement(sql);
+			return preparedStatement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
 }
